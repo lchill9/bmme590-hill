10730f658a8f9440e9fb61efa708cb6d4c528df8
lchill9 <lchill9@unc.edu>
2024-09-12 13:27:23 -0400

    create hw1-2.py
    question 2 from homework 1

---
+++ src/hw1-2.py
@@ -0,0 +1,35 @@
+import networkx as nx
+import random
+
+
+N = 100
+E = 200
+
+G = nx.Graph()
+
+for node in range(1,N+1):
+	G.add_node(node)
+
+for edge in range(1,E+1):
+	B = False
+	while not B:
+		n1 = random.randint(1,N)
+		n2 = random.randint(1,N)
+
+		if (n1, n2) not in G.edges and (n2, n1) not in G.edges:
+			B = True
+			G.add_edge(n1,n2)	
+
+D = nx.density(G)
+deg = nx.degree(G)
+sum_deg = 0
+for node in range(1,N+1):
+        sum_deg = sum_deg + deg(node,2)
+avg_deg = sum_deg/N     
+
+
+print("The density of the network is", D)
+print("The mean degree is", avg_deg)
+print("Degree distribution:",nx.degree_histogram(G))
+
+
